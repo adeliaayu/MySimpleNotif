0a37d37ede50778d9f805966deb887b167f725f6
adeliaayu <adeliaayut53@gmail.com>
2020-12-20 02:22:38 +0700

    add pending intent

--- app/src/main/java/com/dicoding/kotlin/mysimplenotif/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/mysimplenotif/MainActivity.kt
@@ -2,8 +2,11 @@ package com.dicoding.kotlin.mysimplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -17,14 +20,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendNotification(view: View) {
-        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val mNotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_notifications_white_24)
-                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_24))
-                .setContentTitle(resources.getString(R.string.content_title))
-                .setContentText(resources.getString(R.string.content_text))
-                .setSubText(resources.getString(R.string.subtext))
-                .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.ic_notifications_white_24)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    resources,
+                    R.drawable.ic_notifications_white_24
+                )
+            )
+            .setContentTitle(resources.getString(R.string.content_title))
+            .setContentText(resources.getString(R.string.content_text))
+            .setSubText(resources.getString(R.string.subtext))
+            .setAutoCancel(true)
 
         /*
         Untuk android Oreo ke atas perlu menambahkan notification channel
@@ -33,7 +46,11 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
             // Create or Update
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
             channel.description = CHANNEL_NAME
 
             mBuilder.setChannelId(CHANNEL_ID)
